c6acd794b2fc3a1556ee561d55c2eacfa415a4c8
JedlikFK <farkaskrisz9898@gmail.com>
2020-02-25 13:17:49 +0100

    fegyvertárkséz

--- fegyverarzenal/Program.cs
+++ fegyverarzenal/Program.cs
@@ -8,8 +8,18 @@ namespace fegyverarzenal
 {
     class Program
     {
+        enum fegyverek {AK47, MAC10, M4A1S, AWP, GLOCK18, USPS};
         static void Main(string[] args)
         {
+            fegyverek kezemben = fegyverek.AK47;
+            List<fegyverek> taska = new List<fegyverek>();
+            taska.Add(fegyverek.M4A1S);
+            taska.Add(fegyverek.USPS);
+            Console.WriteLine("A fegyverek a táskában:");
+            foreach (fegyverek fegyo in taska)
+            {
+                Console.WriteLine(fegyo);
+            }
             Console.ReadKey();
         }
     }
